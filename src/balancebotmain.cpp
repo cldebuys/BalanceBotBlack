16ef30c73eac93f57b1769440b078701bba37a19
cldebuys <cldebuys@tamu.edu>
2017-05-10 03:33:45 -0500

    Cleaning and Commenting
    will update once more with control matrices defined

--- src/balancebotmain.cpp
+++ src/balancebotmain.cpp
@@ -1,29 +1,22 @@
-// Main Loop for Balance Bot Black
-/* Encoders */
+/* Main Loop for Balance Bot Black */
+	/* Encoders */
 	#include "eqep.h"
 	#include "util.h"
 	#include <cstdlib>
-/* IMU */
+	/* IMU */
 	#include "MPU9250.h"
 	#include <iostream>
 	#include <unistd.h>
-	//#include <math.h>
 	#include <cmath>
 	#include <stdio.h>
 	#include <iomanip>
-/* PWM */
+	/* PWM */
 	#include "PWM.h"
-	//#include "util.h"
-	#include <cstdlib>
-	//#include <iostream>
-/* Motor Driver */
+	/* Motor Driver */
 	#include "MotorController.h"
 	#include "GPIO.h"
-	//#include "PWM.h"
-	//#include <cstdlib>
 	#include <string>
-	//#include <iostream>
-/* Timing */
+	/* Timing */
 	#include <chrono>
 	#include <fstream>
 	#include <sys/time.h>
@@ -31,35 +24,37 @@
 using namespace ZJ;
 using namespace std;
 
-// typedef std::chrono::high_resolution_clock Clock;
-// typedef std::chrono::nanoseconds ns;
-// typedef std::chrono::duration<float> fsec;
+	/* Timing */
     typedef std::chrono::high_resolution_clock Time;
     typedef std::chrono::nanoseconds ns;
     typedef std::chrono::duration<float> fsec;
 	
 int main(){
+// Initialize everything outside the loop
 	cout << "Start\n";
-	/* Initialize the IMU */
+	
+/* Initialize the IMU */
 	MPU9250 imu(2, 0x68);
     imu.setAccRange(MPU9250::PLUSMINUS_16_G);
     imu.setGyroRange(MPU9250::PLUSMINUS_2000_DPS);
 	
-	
-	/* Initialize variables outside the loop */
+/* Loop Variables */
 	bool killSwitch = 1;
 	unsigned long i = 0;
 	
+/* Encoders */
 	EQEP encoder1(0);	//P8_35, P8_33
 	int pos1 = 0;
 	EQEP encoder2(1);	//P8_12, P8_11
-		// GPIO: write failed to open file : Permission denied
 	int pos2 = 0;
 	
+/* PWM */
 	PWM motor1("pwm1a"); //P9_14
 	PWM motor2("pwm1b"); //P9_16
 	GPIO gip1(30); //P9_11
 	GPIO gip2(31); //P9_13
+	
+	/* Control Variables */
 	float volt1 = 0.0;
 	float volt2 = 0.0;
 	float e1 = 0.0;
@@ -68,6 +63,7 @@ int main(){
 	float control2 = 0.0;
 	
 	while (killSwitch){
+		
 	/* Read Encoders */
 		pos1 = encoder1.getPosition();
 		pos2 = encoder2.getPosition();
@@ -77,7 +73,7 @@ int main(){
 		imu.readSensor();
 		// cout << imu.getAccPitch() << "\n";
 		// cout << "\n";
-		// use Felipe's kalman filter here
+		/* use Felipe's kalman filter here */
 		
 		
 	/* Calculate Control Effort */
@@ -86,23 +82,19 @@ int main(){
 		volt2 = 12.0*(e1); // stall voltage is 1V
 		control1 = 92*(1/65.0)*e1 + 8; //percent duty cycle
 		control2 = 92*(1/65.0)*e1 + 8;
-			
+		/* Prevent Saturation */
 		if (abs(e1) >= 60){
 			killSwitch = 0;
 			control1 = 0;
 			control2 = 0;
 		}
 	
+	
 	/* Output Pwm */
 		if (e1 >= 0){
 			gip1.setValue(GPIO::LOW);
 			gip2.setValue(GPIO::LOW);		
 			
-			// if((control1 or control2) < 1/12/100)
-			// {
-				// control1 = 1/12*100;
-				// control2 = 1/12*100;
-			// }
 			motor1.setDutyCyclePercentage(control1);
 			motor2.setDutyCyclePercentage(control2);
 		}
@@ -110,31 +102,15 @@ int main(){
 			gip1.setValue(GPIO::HIGH);
 			gip2.setValue(GPIO::HIGH);
 			
-			// if(( abs(control1) or abs(control2)) < 1/12*100) //this is the minimal control effort dude
-			// {
-				// control1 = 1/12*100;
-				// control2 = 1/12*100;
-			// }
-			
 			motor1.setDutyCyclePercentage(100 - abs(control1));
 			motor2.setDutyCyclePercentage(100 - abs(control2));
 		}
 		
 		cout << imu.getAccPitch() << "	" << control1 << "	" << control2 << "	" << e1<<"\n";
-		// gip1.setValue(GPIO::LOW);
-		// gip2.setValue(GPIO::LOW);
-		// motor1.setDutyCyclePercentage(control1);
-		// motor2.setDutyCyclePercentage(control2);
-		// gip1.setValue(GPIO::HIGH);
-		// gip2.setValue(GPIO::HIGH);
-		// usleep(5000000);
-		// gip1.setValue(GPIO::LOW);
-		// gip2.setValue(GPIO::LOW);
 	
-		
 	 }
 
-///////////////////////////////////////////////////////////////	
+////////////////////* Chrono Sample Code *////////////////////
     // auto t0 = Time::now();
     // auto t1 = Time::now();
     // fsec fs = t1 - t0;
@@ -142,9 +118,10 @@ int main(){
     // std::cout << fs.count() << " s\n";
     // std::cout << d.count() << " ms\n";
     // std::cout << MotorEnCnt/d.count() << " ms\n";
-///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 
-	/* Getting Motor Parameters */
+
+////////////////* Getting Motor Parameters *//////////////////
 	// ofstream out_data;
 	// out_data.open("encoder_rate.dat");
 	// motor.runByVoltage(12.0);
@@ -166,18 +143,13 @@ int main(){
 		// t1 = Time::now();
 		// fsec fs = t1 - t0;
 		// ns d = std::chrono::duration_cast<ns>(fs);
-		
-	/* std::cout << fs.count() << " s\n"; */
-    /* std::cout << d.count() << " ms\n"; */
-    /* std::cout << MotorEnCnt/d.count() << " ms\n"; */
-	
-	/*	encoder_rate = (float)(pos_now - pos_prev) / (float)(chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count());*/
 
 		// encoder_rate = (double)1000*(pos_now - pos_prev)/d.count(); // counts per second
 		
 		// out_data << fs.count() <<"	" << encoder_rate << "\n";
 		// cout << encoder_rate << "	" << pos_now << "	" << pos_prev <<"\n";
 		// t0 = t1;
+		
 		// if (pos_prev == pos_now){
 			// ++iter;
 		// }
@@ -188,23 +160,7 @@ int main(){
 		
 	// }
 	// out_data.close();
-	//////////////////////////////////////////////////////////
-		// cout << "Start\n";
+//////////////////////////////////////////////////////////////
 	
-	// PWM motor1("pwm1a"); //P9_14
-	// PWM motor2("pwm1b"); //P9_16
-	// GPIO gip1(30); //P9_11
-	// GPIO gip2(31); //P9_13
-
-	// gip1.setValue(GPIO::LOW);
-	// gip2.setValue(GPIO::LOW);
-	// motor1.setDutyCyclePercentage(50);
-	// motor2.setDutyCyclePercentage(50);
-	// gip1.setValue(GPIO::HIGH);
-	// gip2.setValue(GPIO::HIGH);
-	// usleep(5000000);
-	// gip1.setValue(GPIO::LOW);
-	// gip2.setValue(GPIO::LOW);
-		
 	return 0;
 }
