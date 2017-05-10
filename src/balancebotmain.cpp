2ba969f60634bc39da94da38ce400fbb34e63076
cldebuys <cldebuys@tamu.edu>
2017-05-10 04:46:47 -0500

    H-Control Added
    Matrices, vectors, and necessary constants initialized.
    Controller added.
    Note: Translational position error is only based on one encoder reading.

--- src/balancebotmain.cpp
+++ src/balancebotmain.cpp
@@ -1,30 +1,33 @@
-/* Main Loop for Balance Bot Black */
-	/* Encoders */
+/* Main Loop for BalanceBotBlack */
+/* Encoders */
 	#include "eqep.h"
 	#include "util.h"
 	#include <cstdlib>
-	/* IMU */
+/* IMU */
 	#include "MPU9250.h"
 	#include <iostream>
 	#include <unistd.h>
 	#include <cmath>
 	#include <stdio.h>
 	#include <iomanip>
-	/* PWM */
+/* PWM */
 	#include "PWM.h"
-	/* Motor Driver */
+/* Motor Driver */
 	#include "MotorController.h"
 	#include "GPIO.h"
 	#include <string>
-	/* Timing */
+/* Timing */
 	#include <chrono>
 	#include <fstream>
 	#include <sys/time.h>
+/* Matrix Math */
+	#include <Eigen/Dense>
+	
+	
+	using namespace ZJ;
+	using namespace std;
 
-using namespace ZJ;
-using namespace std;
-
-	/* Timing */
+/* Timing */
     typedef std::chrono::high_resolution_clock Time;
     typedef std::chrono::nanoseconds ns;
     typedef std::chrono::duration<float> fsec;
@@ -54,14 +57,38 @@ int main(){
 	GPIO gip1(30); //P9_11
 	GPIO gip2(31); //P9_13
 	
-	/* Control Variables */
+/* Control Variables */
 	float volt1 = 0.0;
 	float volt2 = 0.0;
 	float e1 = 0.0;
-	float e2 = 0.0;
+	float pos_d = 0.0;
+	float e_pos = 0.0;
+	float torque_to_percent = 0.0;
 	float control1 = 0.0;
 	float control2 = 0.0;
 	
+/* H-Control Matrices */
+	MatrixXd A(3,3); // use MatrixXf for arbitrary size
+	A << 0, 0, 0,
+		 0, 0, 0,
+		 0, 0, 0;
+		 
+	MatrixXf B(3,2);
+	B << 0, 0,
+		 0, 0,
+		 0, 0;
+		 
+	MatrixXf C(1,3);
+	C << 0, 0, 0;
+	
+	MatrixXf D(1,2);
+	D << 0, 0;
+	
+	Vector2d q(0,0); // column vectors by default
+	Vector2d u(0,0);	
+	Vector2d y(0,0);
+	
+///* THE LOOP OF ETERNAL BALANCE *///
 	while (killSwitch){
 		
 	/* Read Encoders */
@@ -71,25 +98,35 @@ int main(){
 		
 	/* Read IMU */
 		imu.readSensor();
-		// cout << imu.getAccPitch() << "\n";
-		// cout << "\n";
 		/* use Felipe's kalman filter here */
 		
 		
 	/* Calculate Control Effort */
 		e1 = imu.getAccPitch(); // stall voltage is 1V
-		volt1 = 12.0*(e1); // stall voltage is 1V
-		volt2 = 12.0*(e1); // stall voltage is 1V
 		control1 = 92*(1/65.0)*e1 + 8; //percent duty cycle
 		control2 = 92*(1/65.0)*e1 + 8;
+	
+	
+	/* Calculate H-Control Effort */
+		e1 = imu.getAccPitch();
+		e_pos = pos1 - pos_d;
+		u(0) = e1;
+		u(1) = e_pos;
+		
+		y = C*q + D*u;	//   y[k] = C*q[k] + D*u
+		q = A*q + B*u;	// q[k+1] = A*q[k] + B*u
+		
+		control1 = y(0)*torque_to_percent + 8;
+		control2 = y(1)*torque_to_percent + 8;
+	
 		/* Prevent Saturation */
-		if (abs(e1) >= 60){
-			killSwitch = 0;
-			control1 = 0;
-			control2 = 0;
-		}
-	
-	
+			if (abs(e1) >= 60){
+				killSwitch = 0;
+				control1 = 0;
+				control2 = 0;
+			}
+		
+		
 	/* Output Pwm */
 		if (e1 >= 0){
 			gip1.setValue(GPIO::LOW);
@@ -106,7 +143,7 @@ int main(){
 			motor2.setDutyCyclePercentage(100 - abs(control2));
 		}
 		
-		cout << imu.getAccPitch() << "	" << control1 << "	" << control2 << "	" << e1<<"\n";
+		// cout << imu.getAccPitch() << "	" << control1 << "	" << control2 << "	" << e1<<"\n";
 	
 	 }
 
